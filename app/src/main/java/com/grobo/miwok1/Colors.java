2aaf711af3f59a1e49205d9ce427bf9fb93efc9c
amangrobo <amangrobo@gmail.com>
2018-12-03 13:04:46 +0530

    corrected mediaplayer location, release, and onstop.

--- app/src/main/java/com/grobo/miwok1/Colors.java
+++ app/src/main/java/com/grobo/miwok1/Colors.java
@@ -1,19 +1,24 @@
 package com.grobo.miwok1;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class Colors extends AppCompatActivity {
 
+    private MediaPlayer mPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
 
-        ArrayList<Word> words = new ArrayList<Word>();
+        final ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one", "lutti", R.raw.color_red, R.drawable.color_red));
         words.add(new Word("two", "otiiko", R.raw.color_green, R.drawable.color_green));
         words.add(new Word("three", "tolookosu", R.raw.color_brown, R.drawable.color_brown));
@@ -27,5 +32,38 @@ public class Colors extends AppCompatActivity {
 
         WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
         numbersListView.setAdapter(adapter);
+
+        numbersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                releaseMediaPlayer();
+                Word audio = words.get(position);
+                mPlayer = MediaPlayer.create(Colors.this, audio.getAudioResourceId());
+                mPlayer.start();
+
+                mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        releaseMediaPlayer();
+                    }
+                });
+
+            }
+        });
+
     }
+
+    private void releaseMediaPlayer() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
 }
