bb6e8c5270d21ca67872d1e652bcaaa78bec8deb
amangrobo <amangrobo@gmail.com>
2018-12-03 06:11:14 +0530

    Revert "linked mp3 files"
    This reverts commit 1cee71b6d5336ec4f97618bf74bfe38cd9d3a091.

--- app/src/main/java/com/grobo/miwok1/Word.java
+++ app/src/main/java/com/grobo/miwok1/Word.java
@@ -7,30 +7,29 @@ public class Word {
 
     private String mDefaultTranslation;
     private String mMiwokTranslation;
-    private int mAudioResourceId;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
     private static final int NO_IMAGE_PROVIDED = -1;
 
-    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId){
+    public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
-        mAudioResourceId = audioResourceId;
     }
 
-    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId, int imageResourceId) {
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
-        mAudioResourceId = audioResourceId;
         mImageResourceId = imageResourceId;
     }
 
-    public String getDefaultTranslation() {return mDefaultTranslation;}
+    public String getDefaultTranslation(){
+        return mDefaultTranslation;
+    }
 
-    public String getMiwokTranslation() {return mMiwokTranslation;}
+    public String getMiwokTranslation(){
+        return mMiwokTranslation;
+    }
 
-    public int getAudioResourceId() {return mAudioResourceId;}
-
-    public int getImageResourceId() {return mImageResourceId;}
+    public int getImageResourceId() {return mImageResourceId; }
 
     public boolean hasImage(){
         return mImageResourceId != NO_IMAGE_PROVIDED;
--- app/src/main/java/com/grobo/miwok1/WordAdapter.java
+++ app/src/main/java/com/grobo/miwok1/WordAdapter.java
@@ -1,7 +1,6 @@
 package com.grobo.miwok1;
 
 import android.app.Activity;
-import android.media.MediaPlayer;
 import android.support.annotation.DrawableRes;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
@@ -16,7 +15,6 @@ import java.util.ArrayList;
 public class WordAdapter extends ArrayAdapter<Word> {
 
     private int colorResource;
-    MediaPlayer mPlayer;
 
     public WordAdapter(Activity context, ArrayList<Word> words, int colorInput){
         super(context, 0, words);
@@ -31,7 +29,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
         }
 
-        final Word currentWord = getItem(position);
+        Word currentWord = getItem(position);
+
 
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTextView.setText(currentWord.getMiwokTranslation());
@@ -48,15 +47,6 @@ public class WordAdapter extends ArrayAdapter<Word> {
         View textContainer = listItemView.findViewById(R.id.text_container);
         int color = ContextCompat.getColor(getContext(), colorResource);
         textContainer.setBackgroundColor(color);
-
-        textContainer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPlayer = MediaPlayer.create(getContext(), currentWord.getAudioResourceId());
-                mPlayer.start();
-            }
-        });
-
 
         return listItemView;
     }
