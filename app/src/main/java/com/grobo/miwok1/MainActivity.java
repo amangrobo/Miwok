1164f8974ce3cb221ccc921c29314ee4dcb11d72
amangrobo <amangrobo@gmail.com>
2018-12-05 05:47:37 +0530

    added fragments to mainactivity

--- app/src/main/java/com/grobo/miwok1/MainActivity.java
+++ app/src/main/java/com/grobo/miwok1/MainActivity.java
@@ -1,55 +1,28 @@
 package com.grobo.miwok1;
 
-import android.content.Intent;
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
 
-        TextView numbers = (TextView) findViewById(R.id.numbers);
-        numbers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent numbersIntent = new Intent(MainActivity.this, Numbers.class);
-                startActivity(numbersIntent);
-            }
-        });
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        CategoryAdapter pagerAdapter = new CategoryAdapter(this, getSupportFragmentManager());
 
-        TextView colors = (TextView) findViewById(R.id.colors);     /* Color Text View */
-        colors.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent colorsIntent = new Intent(MainActivity.this, Colors.class);
-                startActivity(colorsIntent);
-            }
-        });
+        viewPager.setAdapter(pagerAdapter);
 
-        TextView phrases = (TextView) findViewById(R.id.phrases);
-        phrases.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent phrasesIntent = new Intent(MainActivity.this, Phrases.class);
-                startActivity(phrasesIntent);
-            }
-        });
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
 
-        TextView familyMembers = (TextView) findViewById(R.id.family);
-        familyMembers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent familyIntent = new Intent(MainActivity.this, FamilyMembers.class);
-                startActivity(familyIntent);
-            }
-        });
     }
 
 }
